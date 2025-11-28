26879cdb935e8a37ba4f6c6c72782190b40d8fef
Eike Fabricio <fabricio.eike.s@gmail.com>
2025-11-28 18:07:09 -0300

    chore: usando zod#trim

--- src/tests/questions/options/schema.ts
+++ src/tests/questions/options/schema.ts
@@ -3,16 +3,7 @@ import z from "zod";
 const createTestQuestionOptionSchema = z.object({
   description: z
     .string()
-    .superRefine((val, ctx) => {
-      if (!val?.trim()) {
-        ctx.addIssue({
-          code: "too_small",
-          minimum: 1,
-          origin: "string",
-          message: "Descrição da opção não pode estar vazia",
-        });
-      }
-    })
+    .trim()
     .min(1, { message: "Descrição da opção não pode estar vazia" })
     .max(512, {
       message: "Descrição da opção deve ter no máximo 512 caracteres",
